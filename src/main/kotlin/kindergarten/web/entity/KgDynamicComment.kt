d90a497cb92eeea71e84965487db2a30679abcf6
张瑞宇 <zhangruiyu@hexindai.com>
2017-10-19 20:47:39 +0800

    点赞完成

--- src/main/kotlin/kindergarten/web/entity/KgDynamicComment.kt
+++ src/main/kotlin/kindergarten/web/entity/KgDynamicComment.kt
@@ -4,5 +4,5 @@ import kindergarten.annotation.PoKo
 import java.util.*
 
 @PoKo
-class KgDynamicComment(var id: Long, var userId: Long, var dynamicId: Long, var commentContent: String,
+class KgDynamicComment(var id: Long, var userId: Long, var dynamicId: String, var commentContent: String,
                        var createTime: Date, var groupTag: String, var parentCommentId: String)
--- src/main/kotlin/kindergarten/web/entity/KgDynamicLiked.kt
+++ src/main/kotlin/kindergarten/web/entity/KgDynamicLiked.kt
@@ -4,5 +4,6 @@ package kindergarten.web.entity
  * Created by zhangruiyu on 2017/7/13.
  */
 class KgDynamicLiked {
-    var userId:String?= null
+    var userId: String? = null
+    var nickName: String? = null
 }
