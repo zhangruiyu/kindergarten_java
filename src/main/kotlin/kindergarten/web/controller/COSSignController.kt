167bf78ba995c754e2e19f4bf6f2e7db814d2a0f
zhangruiyu <zhangruiyu@hexindai.com>
2018-06-07 22:03:57 +0800

    costihuan

--- src/main/kotlin/kindergarten/web/controller/COSSignController.kt
+++ src/main/kotlin/kindergarten/web/controller/COSSignController.kt
@@ -1,7 +1,5 @@
 package kindergarten.web.controller
 
-import com.qcloud.cos.sign.Credentials
-import com.qcloud.cos.sign.Sign
 import io.swagger.annotations.ApiImplicitParam
 import io.swagger.annotations.ApiImplicitParams
 import io.swagger.annotations.ApiOperation
@@ -16,6 +14,11 @@ import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.io.File
+import java.util.*
+import java.util.TreeMap
+import com.alibaba.druid.pool.DruidDataSourceFactory.config
+import kindergarten.config.cos.StorageSts
 
 
 /**
@@ -26,26 +29,38 @@ class COSSignController(private val ocsConfig: OCSConfig) {
     val logger = LoggerFactory.getLogger(this.javaClass)
     private val prefix = "/pic"
     //1是发表动态图片
-    @PostMapping(value = [USER_URL+"/cos/periodEffectiveSign"])
+    @PostMapping(value = ["$USER_URL/cos/periodEffectiveSign"])
     @ApiOperation(value = "获取腾讯对象存储sign", notes = "传入上传类型")
     @ApiImplicitParams(ApiImplicitParam(name = "type", value = "0是图片动态 1是视频动态", required = true, dataType = "Int"))
     fun getCoSSign(@RequestParam(required = true) type: Int): ResponseData? {
-        val cred = Credentials(ocsConfig.AppId!!, ocsConfig.SecretId, ocsConfig.SecretKey)
-        val jwt = JwtUserFactory.getJwtUserAfterFilter()
-        val cosPath: String = "$prefix/${CustomConstants.PicType.periodTypes[type]}/${jwt.id}"
-        val expired = System.currentTimeMillis() / 1000 + 3600
-        val periodEffectiveSign = Sign.getPeriodEffectiveSign(ocsConfig.BucketName, cosPath, cred, expired)
-        return COSSignInfo(periodEffectiveSign, cosPath).jsonOk()
-    }
+        val config = TreeMap<String, Any>()
 
-    @PostMapping(value = [USER_URL+"/cos/oneEffectiveSign"])
+// 您的 SecretID
+        config["SecretId"] = ocsConfig.SecretId!!
+// 您的 SecretKey
+        config["SecretKey"] =  ocsConfig.SecretKey!!
+// 临时密钥有效时长，单位是秒，如果没有设置，默认是30分钟
+        config["durationInSeconds"] = 1800
+
+        val credential = StorageSts.getCredential(config).toString()
+//        val credential = StorageSts.getCredential(config)
+//        val expired = System.currentTimeMillis() / 1000 + 3600
+//        val cred = BasicCOSCredentials(ocsConfig.SecretId, ocsConfig.SecretKey)
+//        val jwt = JwtUserFactory.getJwtUserAfterFilter()
+//        val cosPath = "$prefix/${CustomConstants.PicType.periodTypes[type]}/${jwt.id}"
+//        val periodEffectiveSign =  COSSigner().buildAuthorizationStr(HttpMethodName.POST, cosPath, cred, Date(expired))
+        return credential.jsonOk()
+    }
+/*
+    @PostMapping(value = ["$USER_URL/cos/oneEffectiveSign"])
     @ApiOperation(value = "获取腾讯对象存储sign", notes = "传入上传类型")
     @ApiImplicitParams(ApiImplicitParam(name = "type", value = "0是头像", required = true, dataType = "Int"))
     fun getSCoSSign(@RequestParam(required = true) type: Int): ResponseData? {
-        val cred = Credentials(ocsConfig.AppId!!, ocsConfig.SecretId, ocsConfig.SecretKey)
+        val cred = BasicCOSCredentials( ocsConfig.SecretId, ocsConfig.SecretKey)
         val jwt = JwtUserFactory.getJwtUserAfterFilter()
         val cosPath: String = "$prefix/${CustomConstants.PicType.oneTypes[type]}/${jwt.id}.jpg"
-        val periodEffectiveSign = Sign.getOneEffectiveSign(ocsConfig.BucketName, cosPath, cred)
+        val expired = System.currentTimeMillis() / 1000 + 3600
+        val periodEffectiveSign =  COSSigner().buildAuthorizationStr(HttpMethodName.POST, cosPath, cred, Date(expired))
         return COSSignInfo(periodEffectiveSign, cosPath).jsonOk()
-    }
+    }*/
 }
